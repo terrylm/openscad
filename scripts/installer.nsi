1bb75ac728f3a51078cfb8d3011d158acda92872
don bright <hugh.m.bright@gmail.com>
2014-01-05 22:27:18 +0100

    First attempt at getting .mo files working with windows (wine).

--- scripts/installer.nsi
+++ scripts/installer.nsi
@@ -12,6 +12,7 @@ File openscad.exe
 File openscad.com
 File /r /x mingw-cross-env examples
 File /r /x mingw-cross-env libraries
+File /r /x mingw-cross-env po
 ${registerExtension} "$INSTDIR\openscad.exe" ".scad" "OpenSCAD_File"
 CreateShortCut $SMPROGRAMS\OpenSCAD.lnk $INSTDIR\openscad.exe
 WriteUninstaller $INSTDIR\Uninstall.exe
@@ -27,6 +28,7 @@ Delete $SMPROGRAMS\OpenSCAD.lnk
 DeleteRegKey HKEY_LOCAL_MACHINE "SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\OpenSCAD"
 RMDir /r $INSTDIR\examples
 RMDir /r $INSTDIR\libraries\mcad
+RMDir /r $INSTDIR\po
 Delete $INSTDIR\libraries\boxes.scad
 Delete $INSTDIR\libraries\shapes.scad
 RMDir $INSTDIR\libraries
--- scripts/release-common.sh
+++ scripts/release-common.sh
@@ -219,16 +219,19 @@ case $OS in
     MACOSX)
         EXAMPLESDIR=OpenSCAD.app/Contents/Resources/examples
         LIBRARYDIR=OpenSCAD.app/Contents/Resources/libraries
+        TRANSLATIONDIR=OpenSCAD.app/Contents/Resources/po
     ;;
     UNIX_CROSS_WIN)
         EXAMPLESDIR=$DEPLOYDIR/openscad-$VERSION/examples/
         LIBRARYDIR=$DEPLOYDIR/openscad-$VERSION/libraries/
+        TRANSLATIONDIR=$DEPLOYDIR/openscad-$VERSION/po/
         rm -rf $DEPLOYDIR/openscad-$VERSION
         mkdir $DEPLOYDIR/openscad-$VERSION
     ;;
     *)
         EXAMPLESDIR=openscad-$VERSION/examples/
         LIBRARYDIR=openscad-$VERSION/libraries/
+        TRANSLATIONDIR=openscad-$VERSION/po/
         rm -rf openscad-$VERSION
         mkdir openscad-$VERSION
     ;;
@@ -251,6 +254,13 @@ if [ -n $LIBRARYDIR ]; then
   cd $LIBRARYDIR/.. && tar xf $OPENSCADDIR/libraries.tar && cd $OPENSCADDIR
   rm -f libraries.tar
   chmod -R u=rwx,go=r,+X $LIBRARYDIR/*
+fi
+if [ -n $TRANSLATIONDIR ]; then
+  echo $TRANSLATIONDIR
+  mkdir -p $TRANSLATIONDIR
+  tar cvf translations.tar po
+  cd $TRANSLATIONDIR/.. && tar xvf $OPENSCADDIR/translations.tar && cd $OPENSCADDIR
+  rm -f translations.tar
 fi
 
 echo "Creating archive.."
